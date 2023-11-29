512695d5f6d0af1d7b9007dd4ace07f1e765e6ac
MeshachArinze <meshacharinze@gmail.com>
2023-11-29 21:21:05 +0100

    commit to main repo

---
+++ blog/src/app/_data.ts
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    id: "1",
+    name: "bleach",
+    image: {
+      original:
+        "https://m.media-amazon.com/images/M/MV5BZjE0YjVjODQtZGY2NS00MDcyLThhMDAtZGQwMTZiOWNmNjRiXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX1000_.jpg",
+    },
+    kind: "TV",
+    episodes: 366,
+    episodes_aired: 366,
+    score: "7.92",
+  },
+];
