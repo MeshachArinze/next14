cbb846cdfd1c795ead78b3b5cf2fec8cdce08896
MeshachArinze <meshacharinze@gmail.com>
2024-01-16 14:00:18 +0100

    commit to main

--- nextjsportfolio/app/app/api/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-
-  const responseJson = NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-  const responseRedirect = NextResponse.redirect(new URL("/new", request.url));
-  return { responseJson, responseRedirect}
-}
